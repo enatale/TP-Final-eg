875d0cf878ac63e1fd725c5999a90a28b624a859
diegostroppiana <dstroppiana@hotmail.com>
2015-11-25 01:58:11 -0300

    Encabezado comun pa todos

--- TP FINAL/galeria.php
+++ TP FINAL/galeria.php
@@ -9,71 +9,7 @@
     <link href="bootstrap/css/carousel.css" rel="stylesheet">
   </head>
   <body>
-  <div class="container">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>          </button>
-          <a style="margin:0px; padding:0px" class="navbar-brand" href="index.html"><img style="height:100%" src="imagenes/logo.jpg" alt=""></a>        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-           <ul class="nav navbar-nav">
-            <li><a>
-            	<?php
-				session_start(); 
-				if(isset($_SESSION['usuario'])){
-						$nombreSesion = $_SESSION['nombre'];
-						$apellidoSesion = $_SESSION['apellido'];
-						echo('Bienvenido '.$nombreSesion.' '.$apellidoSesion);
-				}?> 
-            </a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-          	<li><a href="index.html">Inicio</a></li>
-            <li class="active"><a href="">Galería</a></li>
-            
-            
-           <?php if(isset($_SESSION['usuario'])){ ?>
-                    <li class="dropdown">
-							  <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reservas <span class="caret"></span></a>
-							  <ul class="dropdown-menu">
-								<li><a href="reserva.php">Reservar instalaciones</a></li>
-								<li><a href="misreservas.php">Mis reservas</a></li>
-              				  </ul>
-                    </li> 
-            		
-					<?php } else{ ?>
-            		
-					<li><a href="disponibilidad.php">Ver disponibilidad</a></li>
-             <?php } ?>
-	   
-            
-            
-            <li><a href="ubicacion.php">Ubicación</a></li>
-            <li><a href="nuestracomision.php">Nuestra comisión</a></li>
-            <li><a href="contacto.php">Contacto</a></li>
-            <?php if(isset($_SESSION['usuario'])){ ?>
-                    <li><a href="modificarcuenta.php">Modificar cuenta</a></li>
-            		<li><a href="cerrarsesion.php">Cerrar sesión</a></li>
-            		<?php } else{ ?>
-            		<li><a href="iniciarsesion.php">Inciar sesión</a></li>
-             <?php } ?>
-			  <?php if(isset($_SESSION['usuario']) and $_SESSION['usuario']=='administrador'){ ?>
-                    <li class="dropdown">
-							  <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Panel administrador <span class="caret"></span></a>
-							  <ul class="dropdown-menu">
-								<li><a href="gestionarusuarios.php">Gestionar usuarios</a></li>
-								<li><a href="gestionarreservas.php">Gestionar reservas</a></li>
-              				  </ul>
-                    </li>
-             <?php } ?>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <?php include'encabezado.php';?>
     <div id="galeria" class="container">
 		<div class="row row-centered">
 			<div class="col-lg-8 col-centered">
--- TP FINAL/reserva.php
+++ TP FINAL/reserva.php
@@ -8,71 +8,7 @@
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a style="margin:0px; padding:0px" class="navbar-brand" href="index.html"><img style="height:100%" src="imagenes/logo.jpg" alt=""></a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-           <ul class="nav navbar-nav">
-            <li> <a>
-				<?php
-				session_start(); 
-				if(isset($_SESSION['usuario'])){
-						$nombreSesion = $_SESSION['nombre'];
-						$apellidoSesion = $_SESSION['apellido'];
-						echo('Bienvenido '.$nombreSesion.' '.$apellidoSesion);
-				}?> 
-            </a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-          	<li><a href="index.html">Inicio</a></li>
-            <li><a href="galeria.php">Galería</a></li>        
-			          
-		    <?php if(isset($_SESSION['usuario'])){ ?>
-                    <li class="dropdown active">
-							  <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Reservas <span class="caret"></span></a>
-							  <ul class="dropdown-menu">
-								<li><a class="active" href="reserva.php">Reservar instalaciones</a></li>
-								<li><a href="misreservas.php">Mis reservas</a></li>
-              				  </ul>
-                    </li> 
-            		
-					<?php } else{ ?>
-            		
-					<li><a href="disponibilidad.php">Ver disponibilidad</a></li>
-             <?php } ?>
-	   
-		   
-            <li><a href="ubicacion.php">Ubicación</a></li>
-            <li><a href="nuestracomision.php">Nuestra comisión</a></li>
-            <li><a href="contacto.php">Contacto</a></li>
-            <?php if(isset($_SESSION['usuario'])){ ?>
-                    <li><a href="modificarcuenta.php">Modificar cuenta</a></li>
-            		<li><a href="cerrarsesion.php">Cerrar sesión</a></li>
-            		<?php } else{ ?>
-            		<li><a href="iniciarsesion.php">Inciar sesión</a></li>
-             <?php } ?>
-			 <?php if(isset($_SESSION['usuario']) and $_SESSION['usuario']=='administrador'){ ?>
-                    <li class="dropdown">
-							  <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Panel administrador <span class="caret"></span></a>
-							  <ul class="dropdown-menu">
-								<li><a href="gestionarusuarios.php">Gestionar usuarios</a></li>
-								<li><a href="gestionarreservas.php">Gestionar reservas</a></li>
-              				  </ul>
-                    </li>
-             <?php } ?>
-			 
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <?php include'encabezado.php';?>
 	
 	<div class="container" style="margin-top:55px">
       <div class="row row-centered">
