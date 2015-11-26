952849662d97417b646dc17180169f2395ec59e6
Esteban Natale <esteban.natale@yahoo.com.ar>
2015-11-25 23:48:53 -0300

    correccion imagenes

--- TP FINAL/galeria.php
+++ TP FINAL/galeria.php
@@ -18,20 +18,20 @@
 					<div id="galeria_miniaturas">
 						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/fachada1.jpg" alt="Fachada1"  />
 						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/fachada2.jpg" alt="Fachada2" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Hall%20e%20ingreso%20a%20los%20Baños%20de%20Frente.JPG" alt="hall e ingreso baños" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Hall%20y%20Secretaría.JPG" alt="Hall y secretaria"/>
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/hall1.jpg" alt="hall e ingreso baños" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Hall2.JPG" alt="Hall y secretaria"/>
 						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Hall.JPG" alt="hall" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal1.jpg" alt="salon principal" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal2.jpg" alt="Salon principal 2" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal3.jpg" alt="Salon principal 3" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal1.JPG" alt="salon principal" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal2.JPG" alt="Salon principal 2" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/SalonPrincipal3.JPG" alt="Salon principal 3" />
 						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Escenario.JPG" alt="Escenario"/> 
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Cocina1.jpg" alt="Cocina 1" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Cocina2.jpg" alt="Cocina 2" /> 
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Interior%20Parrillero,%20Puerta%20de%20ingreso%20a%20Cocina.JPG" alt="Parrillero" /> 
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Interior%20Parrillero%20con%20en%20Depósito.JPG" alt="Parrillero 2" /> 	
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Patio%20y%20Frentes%20de%20Cocina%20y%20Parrillero.JPG" alt="Patio" />      
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Baños1.jpg" alt="Baños 1" />
-						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Baños2.jpg" alt="Baños 2" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Cocina1.JPG" alt="Cocina 1" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Cocina2.JPG" alt="Cocina 2" /> 
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Parrillero1.JPG" alt="Parrillero" /> 
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Parrillero2.JPG" alt="Parrillero 2" /> 	
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Patio.JPG" alt="Patio" />      
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Banos1.JPG" alt="Baños 1" />
+						<img class="miniatura" onClick="javascript:document.getElementById('imgGaleria').src=this.src;" src="imagenes/galeria/Banos2.JPG" alt="Baños 2" />
 									 
 					</div>
 				</div>
