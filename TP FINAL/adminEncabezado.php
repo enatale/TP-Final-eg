dfa6a85a6982900506ad2b3bd9f2c3f3a0f109dc
Esteban Natale <esteban.natale@yahoo.com.ar>
2015-12-03 09:38:28 -0300

    encabezado

---
+++ TP FINAL/adminEncabezado.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+				<div class="navbar-header">
+							  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							  </button>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+								<li><a href="adminInicio.php">Panel de administrador</a></li>
+							  </ul>
+				  <ul class="nav navbar-nav navbar-right">
+					<li><a href="gestionarusuarios.php">Gestionar Usuarios</a></li>
+					<li><a href="gestionarreservas.php">Gestionar reservas</a></li>		
+                    <li><a href="cerrarsesion.php">Cerrar sesión</a></li>
+				  </ul>
+			   </div>
+		
+       </div>
+</nav>
---
+++ TP FINAL/adminInicio.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html style="background-image:none" lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sociedad Italiana de Pujato Gral. Armando Díaz</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+<body style="background-image:none">
+<?php
+	session_start();
+	if(isset($_SESSION['usuario'])&&$_SESSION['usuario']=='administrador'){
+		include('adminEncabezado.php');?>
+		 
+	
+
+<div class="container">
+    	<div class="row row-centered">
+        	<div class="col-md-10 col-centered">
+			
+ <h2 style="text-align:center">Usted se ha logueado como administrador, elija una de las opciones en el menú para gestionar usuarios o reservas</h2>
+            
+            </div>		
+        </div>
+</div>
+
+
+
+
+<?php }
+	else {
+		echo('<h1 style="color:red;text-align:center"> PARA VER ESTA PÁGINA DEBE ESTAR LOGUEADO COMO ADMINISTRADOR</h1>');
+	}
+?>
+
+
+	
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
