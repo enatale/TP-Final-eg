a2ad3f4575e7c43c631f812bbe014a25d6206d72
Esteban Natale <esteban.natale@yahoo.com.ar>
2015-12-08 13:34:18 -0300

    margen para el pie

--- TP FINAL/pie.php
+++ TP FINAL/pie.php
@@ -1,3 +1,4 @@
+<div style="height:50px"></div>
 <footer class="footer navbar-fixed-bottom">
       <div class="container">
         <p class="text-muted">Sociedad Italiana de Pujato General Armando Díaz - Dirección: J.R. Rodríguez 448 - Pujato, Santa Fe, Argentina - Telefono: (03464)-494636</p>
